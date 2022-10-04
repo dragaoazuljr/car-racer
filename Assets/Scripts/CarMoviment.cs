486ce8f1629856d0da172966133a9c410ff2856d
Danillo Moraes <danillo.moraes@outlook.com>
2022-10-04 17:09:07 -0300

    fix: car psychics, feat: start drifiting mechanic

--- Assets/Scripts/CarMoviment.cs
+++ Assets/Scripts/CarMoviment.cs
@@ -27,25 +27,35 @@ public class CarMoviment : MonoBehaviour
     [SerializeField]
     [Range(0f, 10f)]
     public float wheelTurningVelocity;
-
+    [SerializeField]
+    [Range(-0, 2f)]
+    public float tractionControlMultiplier = 1f;
+    [SerializeField]
+    [Range(0, 180)]
+    public int xRotationLimit = 70;
+    [SerializeField]
+    [Range(0, 180)]
+    public int zRotationLimit = 70;
     [SerializeField]
     List<GameObject> carWheels;
+    [SerializeField]
+    [Range(0f, 100000f)]
+    public float jumpForce = 1f;
 
-    Rigidbody rb;
+    Rigidbody carRigidbody;
     float throttleInput;
     float steerInput;
+    bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Transform centerOfMassTransform = centerOfMass.GetComponent<Transform>();
-        rb.centerOfMass = centerOfMassTransform.position;
+        carRigidbody = GetComponent<Rigidbody>();
     }
 
     void OnDrawGizmos() {
-        foreach (GameObject wheel in carWheels) {
-          Transform wheelT = wheel.GetComponent<Transform>();
+       // foreach (GameObject wheel in carWheels) {
+       //   Transform wheelT = wheel.GetComponent<Transform>();
 
           // Gizmos.color = Color.red;
           // Gizmos.DrawLine(wheelT.position, wheelT.position + transform.forward);
@@ -55,65 +65,89 @@ public class CarMoviment : MonoBehaviour
 
          // Gizmos.color = Color.white;
          // Gizmos.DrawLine(wheelT.position, wheelT.position + (Vector3.down * multiplier));
-        }
+        // }
     }
 
     // Update is called once per frame
     void Update()
     { 
-        //position camera behind car
-        Vector3 offset = new Vector3(0f, 5f, -7f);
-
         throttleInput = Input.GetAxisRaw("Acceleration");
         steerInput = Input.GetAxisRaw("Steer");
         bool spaceBar = Input.GetKey(KeyCode.Space);
+        bool jump = Input.GetKey(KeyCode.Q);
 
         if (spaceBar) {
-          transform.Rotate(transform.rotation.x, transform.rotation.y, transform.rotation.z * Time.deltaTime);
+          transform.rotation = Quaternion.identity;
+        }
+
+        if (jump) {
+          isJumping = true;
+        } else {
+          isJumping = false;
         }
     }
 
     void FixedUpdate() {
       RotateFrontWheels(steerInput);
+      Transform centerOfMassTransform = centerOfMass.GetComponent<Transform>();
+      carRigidbody.centerOfMass = centerOfMassTransform.localPosition;
 
-      // Raycast suspention for each wheel;
+      int index = 0;
       foreach (GameObject whellGameObj in carWheels) {
         Transform wheelTransform = whellGameObj.GetComponent<Transform>();
         
-        Ray ray = new Ray(wheelTransform.position, wheelTransform.TransformDirection(Vector3.down));
+        Ray ray = new Ray(wheelTransform.position + (Vector3.up * 0.3f), wheelTransform.TransformDirection(Vector3.down));
         RaycastHit hit;
-        Debug.DrawRay(ray.origin, ray.direction * 0.3f);
 
         if (Physics.Raycast(ray, out hit)) {
           if (hit.distance <= springTravelDistance) {
-            Vector3 tireWorldVelocity = rb.GetPointVelocity(wheelTransform.position);
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance);
+            Vector3 tireWorldVelocity = carRigidbody.GetPointVelocity(wheelTransform.position);
             CalculateSuspentionForce(wheelTransform, tireWorldVelocity, hit.distance);
             CalculateSteeringForce(wheelTransform, tireWorldVelocity);
 
             if (throttleInput != 0) {
-              ApplyThrottleAcceleration(throttleInput);
+              ApplyThrottleAcceleration(throttleInput, wheelTransform, index);
             }
+
+            checkIfJumping(wheelTransform);
           }
         }
+        index++;
       }
     }
 
+    void checkIfJumping(Transform wheelTransform) {
+      if (isJumping) {
+        carRigidbody.AddForceAtPosition((Vector3.up * jumpForce) / 4, wheelTransform.position);
+      }
+    }
+
+    void avoidTippingOver() {
+     // if (transform.rotation.eulerAngles.x > xRotationLimit) {
+     //   transform.rotation = Quaternion.identity;
+     // }
+     // if (transform.rotation.eulerAngles.z > zRotationLimit) {
+     //   transform.rotation = Quaternion.identity;
+     // }
+    }
+
     void CalculateSuspentionForce(Transform wheelTransform, Vector3 tireWorldVelocity, float hitDistance) {
-      Vector3 springDir = wheelTransform.up;
+      Vector3 springDir = Vector3.up;
 
       float offset = springTravelDistance - hitDistance;
       float vel = Vector3.Dot(springDir, tireWorldVelocity);
 
-      float force = (offset * springStrength) - (vel * springDamper);
+      float force = ((offset * springStrength) - (vel * springDamper)) / tireMass;
 
       Ray suspentionForceRay = new Ray(wheelTransform.position, wheelTransform.TransformDirection(springDir));
-      Debug.DrawRay(wheelTransform.position, wheelTransform.TransformDirection(springDir) * force);
+      Debug.DrawRay(wheelTransform.position, wheelTransform.TransformDirection(springDir), Color.green);
 
-      rb.AddForceAtPosition(springDir * force, wheelTransform.position);
+      carRigidbody.AddForceAtPosition(springDir * force, wheelTransform.position);
     }
 
     void CalculateSteeringForce(Transform wheelTransform, Vector3 tireWorldVelocity) {
-      Vector3 steeringDir = wheelTransform.right;
+      Vector3 steeringDir = -wheelTransform.right;
 
       float steeringVel = Vector3.Dot(steeringDir, tireWorldVelocity);
 
@@ -124,34 +158,28 @@ public class CarMoviment : MonoBehaviour
       Vector3 force = steeringDir * tireMass * desiredAccel;
 
       Ray steeringDirectionForceRay = new Ray(wheelTransform.position, wheelTransform.TransformDirection(force));
-      Debug.DrawRay(wheelTransform.position, wheelTransform.TransformDirection(force), Color.red);
+      Debug.DrawRay(wheelTransform.position, force, Color.red);
 
-      rb.AddForceAtPosition(force, wheelTransform.position);
+      carRigidbody.AddForceAtPosition(force, wheelTransform.position);
     }
 
-    void ApplyThrottleAcceleration (float throttleInput) {
-      int index = 0;
-      foreach (GameObject wheel in carWheels) {
-        if (ForwardWheelDrive && index <= 1) {
-          addForceToProperWheel(wheel, throttleInput);
-        } else if (!ForwardWheelDrive && index >= 2) {
-          addForceToProperWheel(wheel, throttleInput);
-        }
-
-        index++;
+    void ApplyThrottleAcceleration (float throttleInput, Transform wheelTransform, float index) {
+      if (ForwardWheelDrive && index <= 1) {
+        addForceToProperWheel(wheelTransform, throttleInput);
+      } else if (!ForwardWheelDrive && index >= 2) {
+        addForceToProperWheel(wheelTransform, throttleInput);
       }
     }
 
-    void addForceToProperWheel(GameObject wheel, float throttleInput) {
-      Transform wheelTransform = wheel.GetComponent<Transform>();
+    void addForceToProperWheel(Transform wheelTransform, float throttleInput) {
       Vector3 wheelPointedDir = wheelTransform.forward;
 
-      Vector3 force = wheelPointedDir * throttleInput * speed;
+      Vector3 force = wheelPointedDir * throttleInput * speed * tireGripFactor;
 
       Ray throttleForceRay = new Ray(wheelTransform.position, wheelTransform.TransformDirection(wheelTransform.forward));
       Debug.DrawRay(wheelTransform.position, force, Color.blue);
 
-      rb.AddForceAtPosition(force, wheelTransform.position);
+      carRigidbody.AddForceAtPosition(force, wheelTransform.position);
     }
 
     void RotateFrontWheels (float input) {
@@ -168,17 +196,16 @@ public class CarMoviment : MonoBehaviour
 
           Debug.DrawRay(wheelFowardRay.origin, wheelFowardRay.direction, Color.yellow);
           Debug.DrawRay(carFowardRay.origin, carFowardRay.direction, Color.magenta);
-          Debug.DrawRay(crossFowardRay.origin, crossFowardRay.direction, Color.white);
 
 
           if (input != 0 && wheelCarFowardDot >= wheelTurningDistance) {
             wheel.Rotate(new Vector3(0, input * wheelTurningVelocity, 0));
           } else if (wheelCarCross.y > 0  && wheelCarFowardDot != 1 && input == 0) {
-            if (wheelCarCross.y >= 0.01f) {
+            if (wheelCarCross.y >= 0.01f && transform.rotation.x >= -0.5f && transform.rotation.x <= 0.5f && transform.rotation.z >= -0.5f && transform.rotation.z <= 0.5f) {
               wheel.Rotate(0, (1 - wheelCarCross.y) * wheelTurningVelocity, 0);
             }
           } else if (wheelCarCross.y < 0 && wheelCarFowardDot != 1 && input == 0) {
-            if (wheelCarCross.y <= -0.01f) {
+            if (wheelCarCross.y <= -0.01f && transform.rotation.x >= -0.5f && transform.rotation.x <= 0.5f && transform.rotation.z >= -0.5f && transform.rotation.z <= 0.5f) {
               wheel.Rotate(0, wheelCarCross.y * wheelTurningVelocity, 0);
             }
           }
